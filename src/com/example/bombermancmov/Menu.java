ceaec810a5340d46e926a5b7d2a7954f743896de
kohle <pkohlmay@gmail.com>
2014-04-24 10:35:54 +0100

    new menu

---
+++ src/com/example/bombermancmov/Menu.java
@@ -0,0 +1,42 @@
+package com.example.bombermancmov;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Menu extends Activity implements OnClickListener{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.menu);
+	    
+	    Button newSingleGame = (Button)findViewById(R.id.bSingle);
+	    Button newMultiGame = (Button)findViewById(R.id.bMulti);
+	    Button options = (Button)findViewById(R.id.bOptions);
+	    Button exit = (Button)findViewById(R.id.bExit);
+	    
+	    newSingleGame.setOnClickListener(this);
+	    newMultiGame.setOnClickListener(this);
+	    options.setOnClickListener(this);
+	    exit.setOnClickListener(this);
+	}
+
+	public void onClick(View view) {
+	    //check which button was clicked with its id
+	    switch(view.getId()) {
+	        case R.id.bExit:
+	            finish();
+	            break;
+	        case R.id.bSingle:
+	            Intent intent = new Intent(this, BomberActivity.class);
+	            this.startActivity(intent);
+	            break;	        
+	    }
+	}
+
+
+
+}
