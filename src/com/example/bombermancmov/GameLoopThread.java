de8f2d5f41a215907d011507a2aa31568c26cc72
Rafael Baltazar <rafael.baltazar@live.com.pt>
2014-05-01 01:07:47 +0100

    Added a neat background to MainMenu

--- src/com/example/bombermancmov/GameLoopThread.java
+++ src/com/example/bombermancmov/GameLoopThread.java
@@ -1,6 +1,7 @@
 package com.example.bombermancmov;
 
 import android.graphics.Canvas;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
@@ -9,25 +10,28 @@ import android.view.SurfaceHolder;
  * the surface view and holder to trigger events every game tick.
  */
 public class GameLoopThread extends Thread {
-
 	private static final String TAG = GameLoopThread.class.getSimpleName();
 
-	// Surface holder that can access the physical surface
+	/** Surface holder that can access the physical surface. */
 	private SurfaceHolder surfaceHolder;
 
-	// The actual view that handles inputs
-	// and draws to the surface
+	/** The actual view that handles inputs and draws to the surface. */
 	private MainGamePanel gamePanel;
 
-	// flag to hold game state
+	/** Flag to hold game state */
 	private boolean running;
 
-	// duration of a frame (in milliseconds)
-	private long frameDuration = 50;
+	/** Frames per second, when game is running and resumed. */
+	private static final int RESUME_FPS = 20;
 
-	public void setRunning(boolean running) {
-		this.running = running;
-	}
+	/** Frames per second, when game is paused. */
+	private static final int PAUSE_FPS = 1;
+
+	/** Maximum number of frames per second. */
+	private int maxFps = RESUME_FPS;
+
+	/** The duration of a frame in milliseconds. */
+	private long frameDuration = 1000 / maxFps;
 
 	public GameLoopThread(SurfaceHolder surfaceHolder, MainGamePanel gamePanel) {
 		super();
@@ -42,8 +46,8 @@ public class GameLoopThread extends Thread {
 	 */
 	@Override
 	public void run() {
+		long beginTime, timeDiff;
 		while (running) {
-			long beginTime = System.currentTimeMillis();
 			// draw step
 			Canvas canvas = null;
 			try {
@@ -51,11 +55,12 @@ public class GameLoopThread extends Thread {
 				// in the surface
 				canvas = this.surfaceHolder.lockCanvas();
 				synchronized (surfaceHolder) {
+					beginTime = SystemClock.uptimeMillis();
 					// update game state
 					// render state to the screen
 					// draws the canvas on the panel
 					if (canvas != null) {
-						this.gamePanel.updateLayer(10);
+						this.gamePanel.updateLayer(frameDuration);
 						this.gamePanel.drawGameModel(canvas);
 					}
 				}
@@ -67,17 +72,35 @@ public class GameLoopThread extends Thread {
 			}
 			// To get constant frame rate, get the time spent
 			// drawing and sleep for the remainder.
-			long timeSpent = System.currentTimeMillis() - beginTime;
+			timeDiff = SystemClock.uptimeMillis() - beginTime;
 			try {
 				// sleep to get constant frame rate
-				if (timeSpent < frameDuration) {
-					long sleepTime = frameDuration - timeSpent;
+				if (timeDiff < frameDuration) {
+					long sleepTime = frameDuration - timeDiff;
 					sleep(sleepTime);
 				}
 			} catch (InterruptedException e) {
 				Log.d(TAG, "Interrupted sleep");
 			}
 		}
-
 	}
+
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+
+	public void unPause() {
+		maxFps = RESUME_FPS;
+		frameDuration = calculateFrameDuration();
+	}
+
+	public void pause() {
+		maxFps = PAUSE_FPS;
+		frameDuration = calculateFrameDuration();
+	}
+
+	private long calculateFrameDuration() {
+		return 1000 / maxFps;
+	}
+	
 }
--- src/com/example/bombermancmov/MainMenuActivity.java
+++ src/com/example/bombermancmov/MainMenuActivity.java
@@ -3,37 +3,48 @@ package com.example.bombermancmov;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
 
-public class MainMenuActivity extends Activity implements OnClickListener{
+public class MainMenuActivity extends Activity {
+	private static final String TAG = MainMenuActivity.class.getSimpleName();
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.menu);
-	    
-	    Button newSingleGame = (Button)findViewById(R.id.bSingle);
-	    Button newMultiGame = (Button)findViewById(R.id.bMulti);
-	    Button options = (Button)findViewById(R.id.bOptions);
-	    Button exit = (Button)findViewById(R.id.bExit);
-	    
-	    newSingleGame.setOnClickListener(this);
-	    newMultiGame.setOnClickListener(this);
-	    options.setOnClickListener(this);
-	    exit.setOnClickListener(this);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu);
+
+		EditText ePlayerName = (EditText) findViewById(R.id.ePlayerName);
+		ePlayerName.setOnKeyListener(sendPlayerName);
 	}
 
-	public void onClick(View view) {
-	    //check which button was clicked with its id
-	    switch(view.getId()) {
-	        case R.id.bExit:
-	            finish();
-	            break;
-	        case R.id.bSingle:
-	            Intent intent = new Intent(this, SingleGameActivity.class);
-	            this.startActivity(intent);
-	            break;	        
-	    }
+	private OnKeyListener sendPlayerName = new OnKeyListener() {
+
+		public boolean onKey(View v, int keyCode, KeyEvent event) {
+			if (event.getAction() == KeyEvent.ACTION_DOWN
+					&& keyCode == KeyEvent.KEYCODE_ENTER) {
+				EditText ev = (EditText) v;
+				String name = ev.getText().toString();
+				ev.setText("");
+
+				Log.d(TAG, "player name: " + name);
+
+				return true;
+			}
+			return false;
+		}
+	};
+
+	public void newSingleGame(View v) {
+		Intent intent = new Intent(this, SingleGameActivity.class);
+		this.startActivity(intent);
 	}
+
+	public void exit(View v) {
+		finish();
+	}
+
 }
