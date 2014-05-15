80a2b745ca849ac1fd72728282f0ca87f4700415
pkohlmay@gmail.com <pkohlmay@gmail.com>
2014-05-15 15:26:59 +0100

    clean up in level.java

--- src/com/example/bombermancmov/model/Level.java
+++ src/com/example/bombermancmov/model/Level.java
@@ -1,10 +1,5 @@
 package com.example.bombermancmov.model;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Level {
 	private String levelName;
 
